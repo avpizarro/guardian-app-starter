3160b7cf8c2112e410dbf27b53e9404857e377a1
avpizarro <avpizarro@live.com>
2021-08-18 17:29:37 +1000

    Fix query string

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,21 +1,19 @@
 import axios from 'axios';
 
-const searchHandler = async(req, res) => {
+const searchHandler = async (req, res) => {
+  try {
+    const searchResults = await axios.get('https://content.guardianapis.com/search?=', req,
+      '&api-key=', process.env.GUARDIAN_API_KEY)
+    const apiResults = searchResults.data.response.results.map(result => {
+      return {
+        title: result.webTitle,
+        url: result.webUrl,
+      }
+    })
 
-  const searchResults = await axios.get('https://content.guardianapis.com/search', {
-    params: {
-      'api-key': process.env.GUARDIAN_API_KEY,
-    }
-  })
+    res.status(200).json(apiResults);
 
-  const apiResults = searchResults.data.response.results.map(result => {
-    return {
-      title: result.webTitle,
-      url: result.webUrl,
-    }
-  })
-
-  res.status(200).json(apiResults);
+  } catch (err) { console.log(err) }
 }
 
 export default searchHandler;
