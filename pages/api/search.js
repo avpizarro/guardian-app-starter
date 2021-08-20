a122e1ea42524f701f4328318acfc7b04f896f2b
avpizarro <avpizarro@live.com>
2021-08-20 14:59:53 +1000

    Fix search function and stop the search if nothingis typed

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const searchHandler = async (req, res) => {
+  console.log("This is the request:",req);
   try {
-    const searchResults = await axios.get('https://content.guardianapis.com/search?=', req,
-      '&api-key=', process.env.GUARDIAN_API_KEY)
+    const searchResults = await axios.get('https://content.guardianapis.com/search?api-key=' + process.env.GUARDIAN_API_KEY+ '&q=' + req.query.term)
     const apiResults = searchResults.data.response.results.map(result => {
+    
       return {
         title: result.webTitle,
         url: result.webUrl,
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,17 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import searchHandler from './api/search';
 
 const IndexPage = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
-
-  // const doSearch = async() => {
-  //   const resultSet = await axios.get('/api/search', {
-  //     params: {
-  //       term: term
-  //     }
-  //   })
-  //   setResults(resultSet.data);
-  // }
+  const [inputHelp, setInputHelp] = useState(false);
 
   const doSearch = async () => {
-    const resultSet = await searchHandler(term);
+    const resultSet = await axios.get('/api/search', {
+      params: {
+        term: term,
+      },
+    });
     setResults(resultSet.data);
   };
 
@@ -24,8 +19,28 @@ const IndexPage = () => {
     <div>
       <h1>Guardian Search</h1>
       <div>
-        <input value={term} onChange={(evt) => setTerm(evt.target.value)} />
-        <button onClick={() => doSearch()}>Search</button>
+        <input
+          value={term}
+          placeholder="Add a word to search"
+          onChange={(evt) => {
+            console.log(evt.target.value);
+            setTerm(evt.target.value);
+            setInputHelp(false)
+          }}
+        />
+        <button
+          onClick={() => {
+            if (term) {
+              doSearch();
+              setInputHelp(false);
+            } else {
+              setInputHelp(true);
+            }
+          }}
+        >
+          Search
+        </button>
+        {inputHelp ? <div>Please add a word</div> : null}
       </div>
       <div>
         <h2>Results</h2>
@@ -44,9 +59,3 @@ const IndexPage = () => {
 };
 
 export default IndexPage;
-
-export const config = {
-  api: {
-    externalResolver: true,
-  },
-}
